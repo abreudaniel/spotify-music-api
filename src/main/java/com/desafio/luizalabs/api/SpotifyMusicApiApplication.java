54e7a1e4016c1175307d5fea8df9bdae62514c45
abreu <abreudaniel@gmail.com>
2025-06-09 17:18:39 +0200

    Start challenge

---
+++ src/main/java/com/desafio/luizalabs/api/SpotifyMusicApiApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.luizalabs.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyMusicApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyMusicApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/luizalabs/api/SpotifyMusicApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.luizalabs.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyMusicApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
